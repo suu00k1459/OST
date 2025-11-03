0a7a60f02494b9321ca9514f0bca44cdb98a2cea
imed-eddine benmadi <benmadi.imed@gmail.com>
2025-11-04 00:59:43 +0100

    removing V0

--- Implementation/sql/timescale_init.sql
+++
@@ -1,8 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS timescaledb;
-CREATE TABLE IF NOT EXISTS federated_metrics (
-ts TIMESTAMPTZ NOT NULL,
-round INT,
-metric TEXT,
-value DOUBLE PRECISION
-);
-SELECT create_hypertable('federated_metrics','ts', if_not_exists => TRUE);
